4d0dff795612e73668fc4745ec37bfc1eb7b0dd0
Echizen1605 <980561079@qq.com>
2017-07-21 15:51:11 +0800

    将html页面解析成字典

--- analyse.py
+++ analyse.py
@@ -2,7 +2,7 @@
 import lxml
 from lxml import etree
 
-with open('k.html','r') as fp:
+with open('k2.html','r') as fp:
 	data = fp.read()
 
 html = etree.HTML(data)
@@ -10,14 +10,29 @@ label_list = {}
 
 def parser(html):
 	tempdict = {}
-	temp_dict = {}
 	count = 0
 	for child in html:
+		temp_dict = {}
 		temp_dict['name'] = child.tag
 		temp_dict['node'] = parser(child)
 		tempdict[count] = temp_dict
 		count += 1
 	return tempdict
 
+def dict_to_tree(tree_dict):
+	if not tree_dict.has_key('node'):
+		return
+	childnode = tree_dict['name']
+	print childnode
+	for key in tree_dict['node'].keys():
+		dict_to_tree(tree_dict['node'][key])
+
 if __name__ == '__main__':
-	print parser(html)
+	thisdict = parser(html)
+	dictk = {}
+	dictk['node'] = thisdict
+	dictk['name'] = 'html'
+	print dictk
+	dict_to_tree(dictk)
+
+
