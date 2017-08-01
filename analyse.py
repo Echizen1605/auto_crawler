a650ecfa7f410e9b77210ab6fcd142c0d9c0eb06
Echizen1605 <980561079@qq.com>
2017-08-01 15:50:35 +0800

    初步实现相似页面数据库内容的筛选

--- analyse.py
+++ analyse.py
@@ -5,13 +5,17 @@ import requests
 import re
 
 reg = re.compile('<script.*?/script>', re.DOTALL)
+reg1 = re.compile('<style.*?</style>', re.DOTALL)
+reg2 = re.compile('<!--.*?-->', re.DOTALL)
 reg_use = re.compile('comment|hidden|javascript|js|css', re.IGNORECASE)
 
 with open('job1.html','rb') as fp:
 	content = fp.read()
-with open('job2.html','r') as fp:
+with open('job2.html','rb') as fp:
 	content1 = fp.read()
 
+content = reg.sub('',reg1.sub('',reg2.sub('',content)))
+content1 = reg.sub('',reg1.sub('',reg2.sub('',content1)))
 html = etree.HTML(content)
 html1 = etree.HTML(content1)
 
@@ -36,11 +40,13 @@ def parser(html, name):
 					temp_dict['name'] += " and @" + key + "='" + temp_dict['attribute'][key] + "'"
 				temp_dict['name'] += "]"
 		temp_dict['path'] = (path_str + str(temp_dict['name']))
+		temp_dict['name'] = str(child.tag)
 		if child.text != None:
 			temp_dict['text'] = child.text.strip()
 		else:
 			temp_dict['text'] = None
 		temp_dict['node'] = parser(child, temp_dict['path'])
+		temp_dict['index'] = count + 1
 		tempdict[count] = temp_dict
 		count += 1
 	return tempdict
@@ -53,16 +59,35 @@ def wrapper_tree(html):
 	dictk['text'] = None
 	dictk['attribute'] = {}
 	dictk['path'] = '/html'
+	dictk['index'] = 1
 	return dictk
 
 def dict_to_tree(tree_dict, tree_list):
 	if not tree_dict.has_key('node'):
 		return
-	tree_tuple = (tree_dict['path'], tree_dict['text'], str(tree_dict['attribute']))
+	tree_tuple = (tree_dict['index'], tree_dict['path'], tree_dict['text'], str(tree_dict['attribute']))
 	if len(reg_use.findall(str(tree_dict['attribute']))) == 0 or len(reg_use.findall(str(tree_dict['path']))) == 0:
 		tree_list.append(tree_tuple)
+	tree_list.append(tree_tuple)
 	for key in tree_dict['node'].keys():
 		dict_to_tree(tree_dict['node'][key], tree_list)
+
+
+def get_ctokens(*dict_tree):
+	ctoken_list = []
+	for tree in dict_tree:
+		ctoken_list.append(tree)
+	print ctoken_list
+
+
+class defined_tuple(object):
+	def __init__(self, mytuple):
+		self.mytuple = mytuple
+	def __eq__(self, other):
+		return self.mytuple[1] == other.mytuple[1] and self.mytuple[2] == other.mytuple[2]
+	def __hash__(self):
+		return hash(self.mytuple[1]) ^ hash(self.mytuple[2])
+
 
 if __name__ == '__main__':
 	list1 = []
@@ -72,20 +97,24 @@ if __name__ == '__main__':
 	dict_to_tree(thisdict, list1)
 	dict_to_tree(thatdict, list2)
 
-	# list1 = list(set([item[0] for item in list1]))
-	# list2 = list(set([item[0] for item in list2]))
-	# print set(list1) - set(list2)
+	myset = set()
+	for i in list1:
+		myset.add(defined_tuple(i))
 
-	sift_list = list(set(list1)-set(list2))
+	hisset = set()
+	for j in list2:
+		hisset.add(defined_tuple(j))
+	
+	ownset = myset - hisset
 
-	for item in sift_list:
-		xp = item[0]
-		try:
-			attr = eval(item[2])
-		except Exception as ex:
-			continue
-		for at in attr.keys():
-			xpa = xp + "/@" + at
-			for sin_item in html.xpath(xpa):
-				print sin_item
-				
+	xpath_set = set()
+	for item in ownset:
+		xpath_set.add(item.mytuple[1])
+	
+	xpath_list = list(xpath_set)
+	for item in xpath_list:
+		print item
+		print len(html.xpath(item))
+		for k in html.xpath(item):
+			if len(k.xpath('text()')) > 0:
+				print k.xpath('text()')[0]
