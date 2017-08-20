aac4e5a7b7ac30d146934cd2fa3e96fa783f4b28
Echizen1605 <980561079@qq.com>
2017-08-20 17:45:04 +0800

    遍历树，锁定特定字段

--- analyse.py
+++ analyse.py
@@ -12,15 +12,20 @@ reg1 = re.compile('<style.*?</style>', re.DOTALL)
 reg2 = re.compile('<!--.*?-->', re.DOTALL)
 reg_use = re.compile('comment|hidden|javascript|js|css', re.IGNORECASE)
 
-# with open('job1.html','rb') as fp:
-# 	content = fp.read()
-# with open('job2.html','rb') as fp:
-# 	content1 = fp.read()
+with open('job1.html','rb') as fp:
+	content = fp.read()
+with open('job2.html','rb') as fp:
+	content1 = fp.read()
 # content = requests.get('https://job.dajie.com/80c1a5e9-668c-4c87-be30-052ffdf021e3.html').content
 # content1 = requests.get('https://job.dajie.com/a8a0c51e-cbe4-4082-aa65-bcdc01389e2e.html').content
 
-content = requests.get('http://my.yingjiesheng.com/job_805115.html').content
-content1 = requests.get('http://my.yingjiesheng.com/job_805112.html').content
+# content = requests.get('http://my.yingjiesheng.com/job_805115.html').content
+# content1 = requests.get('http://my.yingjiesheng.com/job_805112.html').content
+# with open('detail1.html', 'rb') as fp:
+# 	content = fp.read()
+# with open('detail2.html', 'rb') as fp:
+# 	content1 = fp.read()
+
 
 content = reg.sub('',reg1.sub('',reg2.sub('',content)))
 content1 = reg.sub('',reg1.sub('',reg2.sub('',content1)))
@@ -56,7 +61,7 @@ def parser(html, name):
 			temp_dict['text'] = None
 			temp_dict['tokens'] = []
 		temp_dict['node'] = parser(child, temp_dict['path'])
-		temp_dict['index'] = count + 1
+		temp_dict['index'] = html.index(child)
 		tempdict[count] = temp_dict
 		count += 1
 	return tempdict
@@ -79,7 +84,7 @@ def dict_to_tree(tree_dict, tree_list):
 	tree_tuple = (tree_dict['index'], tree_dict['path'], tree_dict['text'], str(tree_dict['attribute']), str(tree_dict['tokens']))
 	if len(reg_use.findall(str(tree_dict['attribute']))) == 0 or len(reg_use.findall(str(tree_dict['path']))) == 0:
 		tree_list.append(tree_tuple)
-	tree_list.append(tree_tuple)
+	# tree_list.append(tree_tuple)
 	for key in tree_dict['node'].keys():
 		dict_to_tree(tree_dict['node'][key], tree_list)
 
@@ -128,16 +133,13 @@ def cos_calculate(tree_list1, tree_list2):
 	myset = set()
 	tree1_set = set(tree_list1)
 	tree2_set = set(tree_list2)
-	# print len(tree1_set)
-	# print len(tree2_set)
-	# print len(tree1_set & tree2_set)
 	myset.union(tree1_set)
 	myset.union(tree2_set)
 	result = len(tree1_set & tree2_set)/math.sqrt((len(tree1_set)*len(tree2_set)))
 	print result
 
 def cmp_html(x, y):
-	return len(x)-len(y)
+	return len(x.split('/'))-len(y.split('/'))
 
 def calculate_prefix(list1):
 	list_union = []
@@ -179,8 +181,104 @@ def calculate_prefix(list1):
 		for path in temp_path_list:
 			path_list.remove(path)
 		count += 1
-	for item in list(set(temp_addition_list)):
-		print item
+	return list(set(temp_addition_list))
+
+def parser_enhance(html, name):
+	tempdict = {}
+	count = 0
+	path_str = name + '/'
+	for child in html:
+		temp_dict = {}
+		temp_dict['name'] = str(child.tag)
+		temp_dict['attribute'] = child.attrib
+		if child.get('id') != None:
+			temp_dict['name'] = str(temp_dict['name']) + "[@id='" + child.get('id') + "']"
+		elif child.get('class') != None:
+			temp_dict['name'] = str(temp_dict['name']) + "[@class='" + child.get('class') + "' and position()=" + str(html.index(child)+1) + "]"
+		elif child.get('name') != None:
+			temp_dict['name'] = str(temp_dict['name']) + "[@name='" + child.get('name') + "' and position()=" + str(html.index(child)+1) + "']"
+		elif len(temp_dict['attribute']) > 0:
+			if type(temp_dict['attribute']) == 'dict':
+				temp_dict['name'] += "[@" + temp_dict['attribute'].keys()[0] + "='" + temp_dict['attribute'][temp_dict['attribute'].keys()[0]] + "'"
+				for key in temp_dict['attribute'].keys()[1:]:
+					temp_dict['name'] += " and @" + key + "='" + temp_dict['attribute'][key] + "'"
+				temp_dict['name'] += " and position()=" + str(html.index(child)+1) + "]"
+		else:
+			temp_dict['name'] += "[position()=" + str(html.index(child)+1) + "]"
+		temp_dict['path'] = (path_str + str(temp_dict['name']))
+		temp_dict['name'] = str(child.tag)
+		if child.text != None:
+			temp_dict['text'] = child.text.strip()
+			temp_dict['tokens'] = jieba.lcut(temp_dict['text'], cut_all=False)
+		else:
+			temp_dict['text'] = None
+			temp_dict['tokens'] = []
+		temp_dict['node'] = parser_enhance(child, temp_dict['path'])
+		tempdict[count] = temp_dict
+		count += 1
+	return tempdict
+
+
+def bianli(tree_dict, sublist):
+	global job_info
+	sublist.append(tree_dict['path'])
+	print tree_dict['text']
+	print tree_dict['attribute']
+	if tree_dict['text'] != None:
+		if len(re.findall(u'下一页', tree_dict['text'])) > 0:
+			job_info['page'] = tree_dict['attribute']['href']
+			job_info['xpath'].append(tree_dict['path'])
+			job_info['text'].append(tree_dict['text'])
+	for key in tree_dict['node'].keys():
+		bianli(tree_dict['node'][key],sublist)
+
+def detect_xpath(given_xpath_list, html):
+	import os.path
+	for single_xpath in given_xpath_list:
+		this_node = html.xpath(single_xpath)
+		for eve_node in this_node:
+			this_dict = parser_enhance(eve_node, single_xpath)
+			my_dict = {}
+			my_dict['node'] = this_dict
+			my_dict['path'] = single_xpath
+			my_dict['text'] = '开始'
+			my_dict['attribute'] = None
+			sublist = []
+			bianli(my_dict, sublist)
+			for item in list(set(sublist)):
+				print item
+			print '***********************************'
+
+		that_node = html1.xpath(single_xpath)
+		for eve_node in that_node:
+			this_dict = parser_enhance(eve_node, single_xpath)
+			my_dict = {}
+			my_dict['node'] = this_dict
+			my_dict['path'] = single_xpath
+			my_dict['text'] = '开始'
+			my_dict['attribute'] = None
+			sublist = []
+			bianli(my_dict, sublist)
+			for item in list(set(sublist)):
+				print item
+			print '***********************************'
+
+def classify(xpath_list, html, cross_list):
+	xpath_info = {}
+	count = 0
+	for single_xpath in xpath_list:
+		templist = []
+		tempdict = {}
+		for every_xpath in cross_list:
+			if every_xpath.startswith(single_xpath):
+				now_html = html.xpath(every_xpath + '/text()')
+				templist = [text_content.strip() for text_content in now_html]
+				tempdict[every_xpath] = templist
+		xpath_info[count] = tempdict
+		cross_list.remove(every_xpath)
+		count += 1
+	print xpath_info.keys()
+	print xpath_info
 
 if __name__ == '__main__':
 	list1 = []
@@ -198,7 +296,7 @@ if __name__ == '__main__':
 	for j in list2:
 		hisset.add(defined_tuple(j))
 	
-	ownset = myset - hisset
+	ownset = (myset - hisset) ^ (hisset - myset)
 
 	xpath_set = set()
 	for item in ownset:
@@ -224,4 +322,17 @@ if __name__ == '__main__':
 	for i in xpath_list:
 		print i
 
-	calculate_prefix(xpath_list)
+	print calculate_prefix(xpath_list)
+	job_info = {}
+	job_info['xpath'] = []
+	job_info['text'] = []
+	detect_xpath(calculate_prefix(xpath_list), html)
+	print job_info
+
+	new_list = calculate_prefix(xpath_list)
+	new_list.sort(cmp_html, reverse=True)
+	print new_list
+
+	classify(new_list, html, xpath_list)
+	
+
